66dd1ee80e1a874b71443bc864e04393d42a09a8
Leandro Lopez <leandro.lopez@3data.net>
2016-05-02 10:18:37 +0200

    Config demo bueno

--- libs/config_demo.php
+++ libs/config_demo.php
@@ -13,13 +13,12 @@ $env = 'dev';
 //$env = 'prod';	
 $config->set('environment', $env);									//entorno dev (desarrollo) o prod (producción)
 $config->set('path',$_SERVER['DOCUMENT_ROOT']);
-$config->SetbaseRef($_SERVER['HTTP_HOST']);
-
+$config->SetbaseRef("http://217.126.85.135:8080/aqualia/www/solutionsdb/");
 
 $config->set('_servidor_bd1', 'localhost');							//url mysql del servidor 1
-$config->set('_database_bd1', 'bd');								//bd del servidor 1
-$config->set('_user_bd1', '');										//user mysql del servidor 1
-$config->set('_password_bd1', '');									//passw del servidor 1
+$config->set('_database_bd1', 'gis_aqualia');								//bd del servidor 1
+$config->set('_user_bd1', 'gisweb');										//user mysql del servidor 1
+$config->set('_password_bd1', '6t7ygv');									//passw del servidor 1
 
 $config->set('_servidor_bd2', 'localhost');							//url mysql del servidor 2
 $config->set('_database_bd2', 'bd');								//bd del servidor 2
@@ -27,7 +26,8 @@ $config->set('_user_bd2', '');										//user mysql del servidor 2
 $config->set('_password_bd2', '');									//passw del servidor 1
 
 $config->set('background', 'contenidos/bg/');
-
+$_SESSION['lang']		= 'es';
+require_once 'includes/'.$_SESSION['lang']."/constants_common.php";
 //WMS url service
-$config->set('urlWMS','http://yourip/geoserver/yourproject/wms');
+$config->set('urlWMS','http://217.126.85.135:8181/geoserver/aqualia/wms');
 ?>
