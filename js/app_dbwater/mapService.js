b6fe14aa80e2ee0a315f1f319cf949fc54dd6b2b
Leandro Lopez <leandro.lopez@3data.net>
2016-07-14 10:43:46 +0200

    Estilo al localizar en dbWater

--- js/app_dbwater/mapService.js
+++ js/app_dbwater/mapService.js
@@ -274,17 +274,17 @@ function map_service($http,$rootScope){
 	
 	function setHighLightStyle(){
 		var _myStroke = new ol.style.Stroke({
-							color : 'rgba(106, 134, 10, 1)',
-							width : 2 
+							color : 'rgba(108, 141, 168, 1)',
+							width : 6 
 						});
 			
-		var _myFill = new ol.style.Fill({
+		/*var _myFill = new ol.style.Fill({
 							color: 'rgba(106, 134, 10, 0.5)'
-						});
+						});*/
 			
 		highLightStyle = new ol.style.Style({
 							stroke : _myStroke,
-							fill : _myFill
+							//fill : _myFill
 						});
 	}
 	
